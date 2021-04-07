da9857cf780a8b4f653c49c608c61c21142845f0
init-zero2023 <santosh.sk11112222@gmail.com>
2021-04-07 22:59:33 +0530

    basic notes app

--- Backend with Rob Percival/Notes-app/app.js
+++ Backend with Rob Percival/Notes-app/app.js
@@ -1,9 +1,67 @@
-// const getNotes = require('./notes');
 const validator = require('validator');
 const chalk = require('chalk');
-// const name = "Santosh";
-// const msg = getNotes();
-// console.log(msg);
-// console.log(validator.isEmail('santosh.co19mailg.ac.in'));
-const greeting = chalk.green.bold.inverse.underline("success");
-console.log(greeting);
+const yargs = require('yargs');
+const fs = require('fs');
+
+// changing version of app with yargs
+yargs.version('1.08.22');
+
+// list of commands with yargs
+yargs.command({
+    command: 'add',
+    description:'Add a new note',
+    builder:{
+        title:{
+            type:'string',
+            required:true,
+           describe: 'Note Title' 
+        },
+        body:{
+            type: 'string',
+            required:true,
+            describe: 'Note body',
+        }
+    },
+    handler: function (argv) {
+            console.log("Title: "+ argv.title);
+            console.log("Body: "+argv.body);
+        },
+});
+yargs.command({
+    command: 'remove',
+    description:'Remove a note',
+    builder:{
+        title:{
+            required:true,
+            describe: "Note Title"
+        }
+    },
+    handler: function (argv) {
+            console.log("Removing "+argv.title);
+        },
+});
+yargs.command({
+    command: 'list',
+    description:'List note',
+    handler: function () {
+            console.log("Listing your notes");
+        },
+});
+yargs.command({
+    command: 'read',
+    description:'Read note',
+    builder:{
+        title:{
+            required:true,
+            describe: "Note Title"
+        }
+    },
+    handler: function (argv) {
+            console.log("Reading "+argv.title);
+        },
+});
+
+// this should be used or use yargs.parse()
+// console.log(yargs.argv);
+
+yargs.parse();
